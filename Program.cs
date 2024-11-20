d56bf566e8109ca64346fc970a2485836a7378de
erdur <erdur@CNOBY>
2024-11-20 23:51:09 +0300

    Proje dosyası ekle.

---
+++ Program.cs
@@ -0,0 +1,31 @@
+﻿using System.Runtime.InteropServices;
+
+namespace AS_221120241020_Oğulcan_Erduran
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Armstrong Sayısını Giriniz");
+            int a, b, c = 0, d;
+            a = Convert.ToInt32(Console.ReadLine());
+            b = a;
+            while (a > 0)
+            {
+                d = a % 10;
+                c = c + d * d * d;
+                a = a / 10;
+
+
+            }
+            if (b==c)
+            {
+                Console.WriteLine("Armstrong Sayı");
+            }
+            else
+                Console.WriteLine(" Armstrong Sayı Değil");
+            Console.WriteLine();
+                Console.ReadLine();
+        }
+    }
+}
